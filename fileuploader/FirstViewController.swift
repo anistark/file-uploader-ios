94c45510d95bd2d38c6a1209c87c9671fbea2d48
Kumar Anirudha <anirudhastark@yahoo.com>
2017-04-03 02:10:35 +0530

    file selector added

--- fileuploader/FirstViewController.swift
+++ fileuploader/FirstViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 import ToastSwiftFramework
+import FileBrowser
 
 class FirstViewController: UIViewController {
     
     @IBAction func selectFile(_ sender: UIButton) {
         self.view.makeToast("This is a piece of toast", duration: 3.0, position: .top)
+        let fileBrowser = FileBrowser()
+        present(fileBrowser, animated: true, completion: nil)
     }
 
 
